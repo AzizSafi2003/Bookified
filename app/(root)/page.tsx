d502a603d00d9bbbe86de326e8b2b490c11c46c8
Abdul Aziz <aziz20031110@gmail.com>
2026-03-05 02:39:10 +0430

    fetch books from database and displaying in the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,17 @@
 import HeroSection from "@/components/HeroSection";
-import { sampleBooks } from "@/lib/constants";
 import BookCard from "@/components/BookCard";
+import { getAllBooks } from "@/lib/actions/book.actions";
 
-const page = () => {
+const page = async () => {
+  const bookResults = await getAllBooks();
+  const books = bookResults.success ? (bookResults.data ?? []) : [];
+
   return (
     <main className="max-w-7xl px-5 mx-auto w-full container">
       <HeroSection />
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-3 md:gap-x-10 gap-y-7 md:gap-y-9">
-        {sampleBooks.map((book) => (
+        {books.map((book) => (
           <BookCard
             key={book._id}
             title={book.title}
